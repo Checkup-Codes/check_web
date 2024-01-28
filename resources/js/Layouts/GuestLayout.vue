5ab286563221db57be8c1709fc720581fed32cc8
Yakup Sarı <61888196+cekapykp@users.noreply.github.com>
2024-01-28 06:19:22 +0300

    feat(check-web) : added landing page link

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,19 +1,27 @@
 <script setup>
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
-import { Link } from '@inertiajs/vue3';
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+import { Link } from "@inertiajs/vue3";
 </script>
 
 <template>
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-        <div>
-            <Link href="/">
-                <ApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
+    <div class="container mx-auto">
+        <div class="flex items-center gap-10 text-md py-3">
+            <Link :href="route('welcome')" class="flex items-center">
+                <ApplicationLogo class="w-20 h-20 fill-current text-gray-900" />
+                <span class="pl-1 text-xl text-blue-800 font-bold"
+                    >Check-up Codes</span
+                >
             </Link>
+
+            <Link :href="route('lp.explore')">Explore</Link>
+            <Link :href="route('lp.products')">Products</Link>
+            <Link :href="route('lp.solutions')">Solutions</Link>
+            <Link :href="route('lp.pricing')">Pricing</Link>
+            <Link :href="route('lp.partners')">Partners</Link>
+            <Link :href="route('lp.resources')">Resources</Link>
         </div>
 
-        <div
-            class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg"
-        >
+        <div class="container">
             <slot />
         </div>
     </div>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { Head, Link } from '@inertiajs/vue3';
+import GuestLayout from '@/Layouts/GuestLayout.vue';
 
 defineProps({
     canLogin: {
@@ -20,38 +21,36 @@ defineProps({
 </script>
 
 <template>
-    <Head title="Welcome" />
+    <Head title="Home" />
 
-    <div
-        class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white"
-    >
-        <div v-if="canLogin" class="sm:fixed sm:top-0 sm:right-0 p-6 text-end">
+   <GuestLayout>
+        <!--<div v-if="canLogin" class="">
             <Link
                 v-if="$page.props.auth.user"
                 :href="route('dashboard')"
-                class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                >Dashboard</Link
-            >
-
+                class=""
+                >Dashboard</Link>
+           
             <template v-else>
                 <Link
                     :href="route('login')"
-                    class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                    class=""
                     >Log in</Link
                 >
-
-                <!--<Link
+               
+                <Link
                     v-if="canRegister"
                     :href="route('register')"
                     class="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
                     >Register</Link
-                >-->
+                >
             </template>
+        </div> -->
+        <div class="">
+            <div class=" text-lg py-6">Site yapma aşaması tatlı tatlı devam etmekte. Open source kodlamaya devam ediyorum. Contributor olmak için <a href="https://discord.gg/9C4JHVjjcd" class="text-pink-700"> DC</a> gel</div>
+            
         </div>
-        <div class="max-w-7xl mx-auto p-6 lg:p-8">
-            <div class="text-white text-2xl">Siteyi kodlamama yardım etmek için  <a href="https://discord.gg/9C4JHVjjcd" class="text-pink-700"> discord</a> gel</div>
-        </div>
-    </div>
+    </GuestLayout>
 </template>
 
 <style>
